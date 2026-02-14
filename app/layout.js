91ce0c07e72c11cdd83c90e864502fe194896c44
chrismereality-cyber <chrismereality-cyber@users.noreply.github.com>
2026-02-14 22:30:26 +0100

    TITAN_V64: Cache Buster // Forcing New Icon Load

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/v-icon.png" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       </head>
