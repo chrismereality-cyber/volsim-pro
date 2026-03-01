84f49f298ece599a1b97d7264cfc0522eeb10890
chrismereality-cyber <chrismereality-cyber@users.noreply.github.com>
2026-03-01 01:00:13 +0100

    V5.8.3: Restoring missing frontend package.json

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,9 @@
 ﻿export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, background: 'black', color: 'white' }}>{children}</body>
+      <body style={{ margin: 0, background: 'black', color: 'white', fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
     </html>
   )
 }
